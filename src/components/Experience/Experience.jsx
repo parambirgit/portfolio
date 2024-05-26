b6d4498f31ea3a6ee5563dceab2720517168fb7d
parambirgit <singhparambir90@gmail.com>
2024-05-26 18:12:41 +0530

    first commit

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./Experience.module.css";
+import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+import { getImageUrl } from "../../utils";
+
+export const Experience = () => {
+  // Filter out the skills you don't want to display
+  const filteredSkills = skills.filter(skill => 
+    !["Node", "GraphQL", "MongoDB", "Figma"].includes(skill.title)
+  );
+
+  return (
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {filteredSkills.map((skill, id) => (
+            <div key={id} className={styles.skill}>
+              <div className={styles.skillImageContainer}>
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              </div>
+              <p>{skill.title}</p>
+            </div>
+          ))}
+        </div>
+        <ul className={styles.history}>
+          {history.map((historyItem, id) => (
+            <li key={id} className={styles.historyItem}>
+              <img
+                src={getImageUrl(historyItem.imageSrc)}
+                alt={`${historyItem.organisation} Logo`}
+              />
+              <div className={styles.historyItemDetails}>
+                <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                <ul>
+                  {historyItem.experiences.map((experience, id) => (
+                    <li key={id}>{experience}</li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
